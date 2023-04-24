ff4f276fdbc21be584a6f4509d1ef498efb98936
papapudge69 <papapudge69@gmail.com>
2023-04-24 18:53:20 +0300

    Moderator Page

--- src/Objects/app.js
+++ src/Objects/app.js
@@ -1,25 +1,3 @@
-import {Site} from "./site";
-import {Sidebar} from "./sidebar";
-
-export class App {
-    constructor(model) {
-        this.model = model
-        this.init()
-    }
-
-    init() {
-        const site = new Site('#site')
-
-        site.render(this.model)
-
-        const updateCallback = newBlock =>{
-            this.model.push(newBlock)
-            site.render(this.model)
-        }
-        new Sidebar('#panel', updateCallback)
-    }
-}
-
 window.addEventListener('scroll', e=> {
     document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
 })
--- src/smth.js
+++
@@ -1,6 +0,0 @@
-import {model} from './model'
-import './styles/main.css'
-import {App} from './Objects/app'
-
-
-new App(model).init()
